39f6cfb63d1d692f33f68b1e156023cbd4842b81
Tai Sakuma <tai.sakuma@gmail.com>
2014-11-24 13:33:32 +0100

    extend the content of PFAOD so as to be able to produce pat::Jets later

--- CMGTools/Production/python/PFAOD/PFAOD_EventContent_RA1_cff.py
+++ CMGTools/Production/python/PFAOD/PFAOD_EventContent_RA1_cff.py
@@ -7,6 +7,28 @@ import copy
 V5_RA1 = copy.copy( V5 )
 V5_RA1.extend(
     [
-    'keep *_ak5CaloJets_*_*',
+        'keep recoCaloJets_ak5CaloJets__*',
+        'keep double_kt6CaloJets_rho_*',
+        'keep recoVertexs_offlinePrimaryVertices__*',
+        'keep *AssociationVector*_ak5JetTracksAssociatorAtVertex__*',
+        'keep recoTracks_generalTracks__*',
+        'keep recoGenParticles_genParticles__*',
+        'keep recoGenJets_ak5GenJets__*',
+        'keep GenEventInfoProduct_generator__*',
+        'keep *CaloTowe*_towerMaker__*',
+        'keep *AssociationVector*_combinedSecondaryVertexBJetTags__*',
+        'keep *AssociationVector*_combinedSecondaryVertexMVABJetTags__*',
+        'keep *AssociationVector*_jetBProbabilityBJetTags__*',
+        'keep *AssociationVector*_jetProbabilityBJetTags__*',
+        'keep *AssociationVector*_simpleSecondaryVertexHighEffBJetTags__*',
+        'keep *AssociationVector*_simpleSecondaryVertexHighPurBJetTags__*',
+        'keep *AssociationVector*_softElectronByPtBJetTags__*',
+        'keep *AssociationVector*_softElectronByIP3dBJetTags__*',
+        'keep *AssociationVector*_softMuonBJetTags__*',
+        'keep *AssociationVector*_softMuonByPtBJetTags__*',
+        'keep *AssociationVector*_softMuonByIP3dBJetTags__*',
+        'keep *AssociationVector*_trackCountingHighEffBJetTags__*',
+        'keep *AssociationVector*_trackCountingHighPurBJetTags__*',
+        'keep *recoJetID*_ak5JetID__*',
     ]
     )
